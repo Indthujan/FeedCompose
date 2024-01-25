6d79c7464342f1a18178b9292de12a1d6c79a7bc
indthujan <indthu2012@gmail.com>
2024-01-25 23:03:49 +0800

    appbar - textfield

---
+++ theme/src/main/java/com/feed/theme/components/PreviewAnno.kt
@@ -0,0 +1,18 @@
+package com.feed.theme.components
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Light",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+
+@Preview(
+    name = "Night",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+
+annotation class NightLightMode
